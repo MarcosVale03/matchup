5902174e9a7a8549741870f18f7ce90cad149580
Gerald Hill <111713369+geralddhill@users.noreply.github.com>
2025-12-14 16:29:41 -0800

    Moves db.types.ts to lib/

--- server/db/client.ts
+++ server/db/client.ts
@@ -1,5 +1,5 @@
 import { createBrowserClient } from "@supabase/ssr";
-import {Database} from "@/server/db/db.types";
+import {Database} from "@/lib/types/db.types";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
