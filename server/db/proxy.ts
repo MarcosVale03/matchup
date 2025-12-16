d9804d47bc883b1d07f451b4f1cd68b3db03c173
Gerald Hill <111713369+geralddhill@users.noreply.github.com>
2025-12-15 23:09:10 -0800

    Allows non logged in users to view the /tournaments page

--- server/db/proxy.ts
+++ server/db/proxy.ts
@@ -38,19 +38,10 @@ export async function updateSession(request: NextRequest) {
 
     const user = data?.claims
     const {pathname} = request.nextUrl;
-    if (
-        !user &&
-        pathname == '/tournaments'
-    ) {
-        // no user
-        const url = request.nextUrl.clone()
-        url.pathname = '/login'
-        return NextResponse.redirect(url)
-    }
 
     if (
         !user &&
-        request.nextUrl.pathname.startsWith('/admin')
+        pathname.startsWith('/admin')
     ) {
         // no user, potentially respond by redirecting the user to the login page
         const url = request.nextUrl.clone()
