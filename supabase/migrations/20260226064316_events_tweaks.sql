5ea7259b5d400ec664ec58a68f1f806bea7205e3
Gerald Hill <111713369+geralddhill@users.noreply.github.com>
2026-02-28 12:33:23 -0800

    Updated tournaments and admins table rls policies
    - Added has_permission_level postgres function that returns true if a given play has a permission level within a given tournament

--- supabase/migrations/20260226064316_events_tweaks.sql
+++ supabase/migrations/20260226064316_events_tweaks.sql
@@ -8,3 +8,95 @@ create policy "events_select"
                  using ((tournament_id = ( SELECT tournaments.id
                  FROM public.tournaments
                  WHERE ((tournaments.is_public = true) OR (tournaments.owner = auth.uid())))));
+CREATE OR REPLACE FUNCTION public.has_permission_level(tid bigint, uid uuid, plevel integer)
+ RETURNS boolean
+ LANGUAGE plpgsql
+AS $function$DECLARE
+  level integer;
+BEGIN
+SELECT permission_level
+FROM public.admins
+WHERE (tournament_id = tid AND user_id = uid)
+    INTO level;
+
+IF level IS NULL THEN
+    RETURN false;
+END IF;
+
+  IF level <= plevel THEN
+    RETURN true;
+END IF;
+
+RETURN false;
+END;$function$
+;
+
+
+drop policy "Enable insert for tournament owners only" on "public"."admins";
+
+drop policy "Enable read access for all users" on "public"."admins";
+
+drop policy "Enable delete for authenticated users based on owner" on "public"."tournaments";
+
+drop policy "Enable insert for authenticated users based on owner" on "public"."tournaments";
+
+drop policy "Enable read access of public tournaments for all users" on "public"."tournaments";
+
+drop policy "Enable update for authenticated users based on owner" on "public"."tournaments";
+
+
+  create policy "admins_insert"
+  on "public"."admins"
+  as permissive
+  for insert
+  to authenticated
+with check ((( SELECT tournaments.owner
+   FROM public.tournaments
+  WHERE (tournaments.id = admins.tournament_id)) = auth.uid()));
+
+
+
+  create policy "admins_select"
+  on "public"."admins"
+  as permissive
+  for select
+                                                          to public
+                                                          using (((user_id = auth.uid()) OR (tournament_id = ( SELECT tournaments.id
+                                                          FROM public.tournaments
+                                                          WHERE ((tournaments.is_public = true) OR (tournaments.owner = auth.uid()))))));
+
+
+
+create policy "tournaments_delete"
+  on "public"."tournaments"
+  as permissive
+  for delete
+to authenticated
+using ((auth.uid() = owner));
+
+
+
+  create policy "tournaments_insert"
+  on "public"."tournaments"
+  as permissive
+  for insert
+  to authenticated
+with check ((auth.uid() = owner));
+
+
+
+  create policy "tournaments_select"
+  on "public"."tournaments"
+  as permissive
+  for select
+                            to public
+                            using (((is_public = true) OR (owner = auth.uid()) OR public.has_permission_level(id, auth.uid(), 4)));
+
+
+
+create policy "tournaments_update"
+  on "public"."tournaments"
+  as permissive
+  for update
+                 to authenticated
+                 using (public.has_permission_level(id, auth.uid(), 1));
