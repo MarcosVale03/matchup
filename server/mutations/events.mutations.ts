da7466afecf40929b588ecd08d99561c2d91d3e8
Gerald Hill <111713369+geralddhill@users.noreply.github.com>
2026-02-28 13:37:43 -0800

    Added insertEvent() function
    - Updated db function and zod schema for inserting events
    - Added rls policies for events, bracket_phases, phase_groups, bracket_types, video_games, gaming_platforms, and event_video_games tables

--- server/mutations/events.mutations.ts
+++ server/mutations/events.mutations.ts
@@ -1,10 +1,10 @@
 'use server'
 
 import * as z from "zod";
-import {DateToISOStr, LocationSchema} from "@/lib/types/zod.types";
+import {DateToISOStr} from "@/lib/types/zod.types";
 import {cookies} from "next/headers";
 import {createClient} from "@/server/db/server";
-import {SupabaseClient} from "@supabase/supabase-js";
+import {MutationResponse} from "@/lib/types/types";
 
 const EventInsertSchema = z.object({
     tournament_id: z.number(),
@@ -14,39 +14,21 @@ const EventInsertSchema = z.object({
         end_time: DateToISOStr
     }).refine(data => data.start_time < data.end_time, {error: "Start time must be before end time"}),
     price: z.number(),
-    video_game: z.string(),
-    platform: z.string(),
+    game_and_platform: z.object({
+        video_game: z.string(),
+        platform: z.string()
+    }),
     teams_allowed: z.boolean(),
     max_team_size: z.optional(z.number()),
-    is_online: z.boolean(),
-    location: z.optional(LocationSchema),
-}).superRefine((data, ctx) => {
-    if (!(data.is_online || data.location)) {
-        ctx.addIssue({
-            code: "custom",
-            message: "Need location for offline event",
-            input: data.location,
-            path: ["location"]
-        })
-    }
-
-    if (data.teams_allowed && !data.max_team_size) {
-        ctx.addIssue({
-            code: "custom",
-            message: "Need max team size for team event",
-            input: data.max_team_size,
-            path: ["max_team_size"]
-        })
-    }
-})
+}).refine(data => !data.teams_allowed || data.max_team_size, {error: "You must specify a max team size for a team event", path: ["max_team_size"]})
+    .refine(data => isGamePlatformValid(data.game_and_platform.video_game, data.game_and_platform.platform), {error: "Invalid video game and platform combination", path:["game_and_platform"]})
 
 export type EventInsertErrors = {
     tournament_id?: string[]
     name?: string[],
     times?: string[],
     price?: string[],
-    video_game?: string[],
-    platform?: string[],
+    game_and_platform?: string[],
     teams_allowed?: string[],
     max_team_size?: string[],
     is_online?: string[],
@@ -54,27 +36,31 @@ export type EventInsertErrors = {
 }
 
 /**
- * @deprecated DO NOT USE! NOT FINISHED
- * @param tournamentId
- * @param name
- * @param startTime
- * @param endTime
- * @param price
- * @param videoGame
- * @param platform
- * @param teamsAllowed
- * @param maxTeamSize
+ * Inserts a new event for the given tournament in the database. Also inserts a starting bracket phase and phase group
+ * @param tournamentId - The id of the tournament that contains the new event
+ * @param name - The name of the event
+ * @param startTime - The start time of the event
+ * @param endTime - The end time of the event. Must be later than the start time
+ * @param price - The entry fee for the event
+ * @param videoGame - The video game that the event is played on
+ * @param platform - The gaming platform/console that the event is played on
+ * @param teamsAllowed - True if teams are allowed in the tournament. False if teams are not allowed
+ * @param maxTeamSize - The maximum size for a team within a tournament. Must be specified if teamsAllowed is true
+ *
+ * @returns Response from mutation
+ * @returns success - True if the DB mutation is successful. False if it fails.
+ * @returns formErrors - Optional. Contains error messages that apply to the whole form.
+ * @returns fieldErrors - Optional. Contains error messages that apply to specific parts of the form.
+ * @returns data - Returns event id of inserted event if success is true
  */
-/*
+
 export async function insertEvent(tournamentId: number, name: string, startTime: Date, endTime: Date, price: number,
-                                  videoGame: string, platform: string, teamsAllowed: boolean, maxTeamSize?: number) {
-    /!*
-     * FUNCTION NOT READY FOR USE
-     *!/
+                                  videoGame: string, platform: string, teamsAllowed: boolean, maxTeamSize?: number):
+    Promise<MutationResponse<number, EventInsertErrors>> {
     const cookieStore = await cookies()
     const supabase = await createClient(cookieStore)
 
-    const result = EventInsertSchema.safeParse({
+    const result = await EventInsertSchema.safeParseAsync({
         tournament_id: tournamentId,
         name: name,
         times: {
@@ -82,8 +68,10 @@ export async function insertEvent(tournamentId: number, name: string, startTime:
             end_time: endTime,
         },
         price: price,
-        video_game: videoGame,
-        platform: platform,
+        game_and_platform: {
+            video_game: videoGame,
+            platform: platform,
+        },
         teams_allowed: teamsAllowed,
         max_team_size: maxTeamSize,
     })
@@ -97,25 +85,16 @@ export async function insertEvent(tournamentId: number, name: string, startTime:
         }
     }
 
-    if (!await isGamePlatformValid(supabase, result.data.video_game, result.data.platform)) {
-        throw new Error("Video game and platform combination not valid.")
-    }
-
-    const {error} = await supabase.rpc('insert_event', {
-        tournament_id: result.data.tournament_id,
-        name: result.data.name,
-        start_time: result.data.times.start_time,
-        end_time: result.data.times.end_time,
-        price: result.data.price,
-        video_game: result.data.video_game,
-        platform: result.data.platform,
-        teams_allowed: result.data.teams_allowed,
-        is_online: result.data.is_online,
-        max_team_size: result.data.max_team_size,
-        place_id: result.data.location?.maps_place_id,
-        address: result.data.location?.address,
-        latitude: result.data.location?.latitude,
-        longitude: result.data.location?.longitude
+    const {data, error} = await supabase.rpc('insert_event', {
+        e_tournament_id: result.data.tournament_id,
+        e_name: result.data.name,
+        e_start_time: result.data.times.start_time,
+        e_end_time: result.data.times.end_time,
+        e_price: result.data.price,
+        e_video_game_name: result.data.game_and_platform.video_game,
+        e_gaming_platform_name: result.data.game_and_platform.platform,
+        e_teams_allowed: result.data.teams_allowed,
+        e_max_team_size: result.data.max_team_size,
     })
 
     if (error) {
@@ -123,15 +102,19 @@ export async function insertEvent(tournamentId: number, name: string, startTime:
     }
 
     return {
-        success: true
+        success: true,
+        data: data
     }
 }
 
-async function isGamePlatformValid(supabase: SupabaseClient, videoGame: string, platform: string) {
+async function isGamePlatformValid(videoGame: string, platform: string) {
+    const cookieStore = await cookies()
+    const supabase = await createClient(cookieStore)
+
     const {data, error} = await supabase.from('event_video_games').select()
         .eq('video_game_name', videoGame).eq('gaming_platform_name', platform)
     if (error || !data) {
         throw new Error("DB error while querying event_video_games: " + error.details + " " + error.message)
     }
     return data.length > 0
-}*/
+}
--- supabase/migrations/20260226064316_events_tweaks.sql
+++ supabase/migrations/20260226064316_events_tweaks.sql
@@ -8,6 +8,37 @@ create policy "events_select"
                  using ((tournament_id = ( SELECT tournaments.id
                  FROM public.tournaments
                  WHERE ((tournaments.is_public = true) OR (tournaments.owner = auth.uid())))));
+
+
+CREATE OR REPLACE FUNCTION public.insert_event(e_tournament_id bigint, e_name character varying,
+    e_start_time timestamp with time zone, e_end_time timestamp with time zone, e_price numeric(12, 2),
+    e_video_game_name text, e_gaming_platform_name text, e_teams_allowed boolean, e_max_team_size smallint default null)
+    RETURNS smallint
+    LANGUAGE plpgsql
+    SET search_path to ''
+AS $$
+DECLARE
+    e_id smallint;
+    bp_id smallint;
+BEGIN
+    INSERT INTO public.events (tournament_id, name, start_time, end_time, price, video_game_name, gaming_platform_name, teams_allowed, max_team_size)
+    VALUES (e_tournament_id, e_name, e_start_time, e_end_time, e_price, e_video_game_name, e_gaming_platform_name, e_teams_allowed, e_max_team_size)
+        RETURNING id
+    INTO e_id;
+
+    INSERT INTO public.bracket_phases (tournament_id, event_id, name, num_progressing_per_group, bracket_type_name, next_phase_id)
+    VALUES (e_tournament_id, e_id, 'Bracket', 1, 'Single Elimination', NULL)
+        RETURNING id
+    INTO bp_id;
+
+    INSERT INTO public.phase_groups (identifier, tournament_id, event_id, bracket_phase_id, wave_identifier)
+    VALUES ('Bracket', e_tournament_id, e_id, bp_id, NULL);
+
+    RETURN e_id;
+END;$$;
+
+alter table "public"."phase_groups" add constraint "phase_groups_bracket_phase_id_unique" UNIQUE (identifier, tournament_id, event_id, bracket_phase_id);
+
 CREATE OR REPLACE FUNCTION public.has_permission_level(tid bigint, uid uuid, plevel integer)
  RETURNS boolean
  LANGUAGE plpgsql
@@ -63,7 +94,7 @@ with check ((( SELECT tournaments.owner
                                                           to public
                                                           using (((user_id = auth.uid()) OR (tournament_id = ( SELECT tournaments.id
                                                           FROM public.tournaments
-                                                          WHERE ((tournaments.is_public = true) OR (tournaments.owner = auth.uid()))))));
+                                                          WHERE ((tournaments.is_public = true)))) OR has_permission_level(tournament_id, auth.uid(), 4)));
 
 
 
@@ -100,3 +131,100 @@ create policy "tournaments_update"
   for update
                  to authenticated
                  using (public.has_permission_level(id, auth.uid(), 1));
+
+drop policy "events_select" on "public"."events";
+
+
+  create policy "bracket_phases_insert"
+  on "public"."bracket_phases"
+  as permissive
+  for insert
+  to authenticated
+with check (public.has_permission_level(tournament_id, auth.uid(), 1));
+
+
+
+  create policy "bracket_phases_select"
+  on "public"."bracket_phases"
+  as permissive
+  for select
+                                 to public
+                                 using (((tournament_id = ( SELECT tournaments.id
+                                 FROM public.tournaments
+                                 WHERE (tournaments.is_public = true))) OR public.has_permission_level(tournament_id, auth.uid(), 4)));
+
+
+
+create policy "bracket_types_select"
+  on "public"."bracket_types"
+  as permissive
+  for select
+                 to public
+                 using (true);
+
+
+
+create policy "event_video_games_select"
+  on "public"."event_video_games"
+  as permissive
+  for select
+                 to public
+                 using (true);
+
+
+
+create policy "events_insert"
+  on "public"."events"
+  as permissive
+  for insert
+  to authenticated
+with check (public.has_permission_level(tournament_id, auth.uid(), 1));
+
+
+
+  create policy "gaming_platforms_select"
+  on "public"."gaming_platforms"
+  as permissive
+  for select
+                          to public
+                          using (true);
+
+
+
+create policy "phase_groups_insert"
+  on "public"."phase_groups"
+  as permissive
+  for insert
+  to authenticated
+with check (public.has_permission_level(tournament_id, auth.uid(), 1));
+
+
+
+  create policy "phase_groups_select"
+  on "public"."phase_groups"
+  as permissive
+  for select
+                          to public
+                          using (((tournament_id = ( SELECT tournaments.id
+                          FROM public.tournaments
+                          WHERE (tournaments.is_public = true))) OR public.has_permission_level(tournament_id, auth.uid(), 4)));
+
+
+
+create policy "video_games_select"
+  on "public"."video_games"
+  as permissive
+  for select
+                 to public
+                 using (true);
+
+
+
+create policy "events_select"
+  on "public"."events"
+  as permissive
+  for select
+                 to public
+                 using (((tournament_id = ( SELECT tournaments.id
+                 FROM public.tournaments
+                 WHERE (tournaments.is_public = true))) OR public.has_permission_level(tournament_id, auth.uid(), 4)));
